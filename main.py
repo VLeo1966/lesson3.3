3a36dae399703c6a9a510430be21293e19ac99cb
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-07-05 18:04:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра тир")
+
 icon = pygame.image.load("img/tir.jpg")
 pygame.display.set_icon(icon)
 
