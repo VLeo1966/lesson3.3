69474bc28091da7fafb092a9c822fcf4079ca60d
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-07-05 17:31:29 +0300

    add new cost

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра тир")
+icon = pygame.image.load("icon.png")
+
 running = True
 while running:
     pass
